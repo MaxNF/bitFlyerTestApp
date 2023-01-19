7e8875035ca5a964a43a4d2e0ca8f1855fdb0630
MaxNF <poeismylife@gmail.com>
2023-01-20 00:21:24 +0900

    remove UserListInteractor

--- app/src/main/java/com/bitflyer/testapp/domain/userlist/UserListInteractor.kt
+++
@@ -1,5 +0,0 @@
-package com.bitflyer.testapp.domain.userlist
-
-class UserListInteractor(private val repo: UserListRepository) {
-    suspend fun getUsers(fromId: Int, count: Int) = repo.getUsers(fromId, count)
-}
