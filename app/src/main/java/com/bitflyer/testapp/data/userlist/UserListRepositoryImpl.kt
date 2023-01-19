52bbe14f71525663bd285d7db96def418519e602
MaxNF <poeismylife@gmail.com>
2023-01-20 00:21:51 +0900

    add missing @Inject

--- app/src/main/java/com/bitflyer/testapp/data/userlist/UserListRepositoryImpl.kt
+++ app/src/main/java/com/bitflyer/testapp/data/userlist/UserListRepositoryImpl.kt
@@ -5,8 +5,9 @@ import com.bitflyer.testapp.data.CallResult
 import com.bitflyer.testapp.data.network.GithubNetworkApi
 import com.bitflyer.testapp.data.userlist.dto.UserBrief
 import com.bitflyer.testapp.domain.userlist.UserListRepository
+import javax.inject.Inject
 
-class UserListRepositoryImpl(private val api: GithubNetworkApi): BaseRepo(), UserListRepository {
+class UserListRepositoryImpl @Inject constructor(private val api: GithubNetworkApi) : BaseRepo(), UserListRepository {
     override suspend fun getUsers(fromId: Int, count: Int): CallResult<List<UserBrief>> = safeApiCall {
         api.getUsers(fromId, count)
     }
--- app/src/main/java/com/bitflyer/testapp/ui/userlist/mapper/UserListModelMapper.kt
+++ app/src/main/java/com/bitflyer/testapp/ui/userlist/mapper/UserListModelMapper.kt
@@ -3,7 +3,8 @@ package com.bitflyer.testapp.ui.userlist.mapper
 import com.bitflyer.testapp.data.userlist.dto.UserBrief
 import com.bitflyer.testapp.ui.BaseMapper
 import com.bitflyer.testapp.ui.userlist.model.UserListModel
+import javax.inject.Inject
 
-class UserListModelMapper : BaseMapper<UserBrief, UserListModel> {
+class UserListModelMapper @Inject constructor(): BaseMapper<UserBrief, UserListModel> {
     override fun map(dto: UserBrief): UserListModel = UserListModel(dto.id, dto.login, dto.avatarUrl)
 }
