8552813e71b8cbe5d9a7a8510d6c28fbcaeea264
MaxNF <poeismylife@gmail.com>
2023-01-20 00:35:23 +0900

    modify di modules

--- app/src/main/java/com/bitflyer/testapp/di/Modules.kt
+++ app/src/main/java/com/bitflyer/testapp/di/Modules.kt
@@ -1,6 +1,14 @@
 package com.bitflyer.testapp.di
 
+import com.bitflyer.testapp.data.network.GithubNetworkApi
+import com.bitflyer.testapp.data.userlist.UserListRepositoryImpl
+import com.bitflyer.testapp.data.userlist.dto.UserBrief
+import com.bitflyer.testapp.domain.userlist.UserListRepository
+import com.bitflyer.testapp.ui.BaseMapper
+import com.bitflyer.testapp.ui.userlist.mapper.UserListModelMapper
+import com.bitflyer.testapp.ui.userlist.model.UserListModel
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,14 +24,16 @@ import javax.inject.Singleton
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class MapperModule {
-    //TODO
+    @Binds
+    abstract fun provideUserListModelMapper(mapper: UserListModelMapper): BaseMapper<UserBrief, UserListModel>
 
 }
 
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class RepositoryModule {
-    //TODO
+    @Binds
+    abstract fun provideUserListRepo(userListRepository: UserListRepositoryImpl): UserListRepository
 }
 
 @Module
@@ -35,12 +45,11 @@ class NetworkModule {
     @Provides
     fun provideRetrofitClient(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://random-word-api.herokuapp.com/")
+            .baseUrl("https://api.github.com")
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
 
-    //TODO
-//    @Provides
-//    fun provideApi(retrofit: Retrofit) =
+    @Provides
+    fun provideGithubApi(retrofit: Retrofit) = GithubNetworkApi.create(retrofit)
 }
