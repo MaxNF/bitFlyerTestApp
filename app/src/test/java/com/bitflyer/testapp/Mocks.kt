43f7f899016315209fdb7b1447a06a38cdd07d0e
MaxNF <poeismylife@gmail.com>
2023-01-21 21:17:26 +0900

    create UserDetailsViewModel tests

--- app/src/test/java/com/bitflyer/testapp/Mocks.kt
+++ app/src/test/java/com/bitflyer/testapp/Mocks.kt
@@ -3,6 +3,7 @@ package com.bitflyer.testapp
 import com.bitflyer.testapp.data.userdetails.dto.UserDetails
 import com.bitflyer.testapp.data.userlist.dto.UserBrief
 import com.bitflyer.testapp.domain.userlist.entity.UserBriefEntity
+import com.bitflyer.testapp.ui.userdetails.model.UserDetailsModel
 
 val userBriefMock
     get() = UserBrief(1, "login", "avatar")
@@ -30,4 +31,20 @@ val userDetailsMock
         1,
         2,
         3
+    )
+
+val userDetailsModelMock
+    get() = UserDetailsModel(
+        "login",
+        "avatarUrl",
+        "name",
+        "company",
+        "blogUrl",
+        "location",
+        "email",
+        "twitter",
+        "bio",
+        "1",
+        "2",
+        "3"
     )
---
+++ app/src/test/java/com/bitflyer/testapp/ui/userdetails/UserDetailsViewModelTest.kt
@@ -0,0 +1,81 @@
+package com.bitflyer.testapp.ui.userdetails
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.lifecycle.SavedStateHandle
+import com.bitflyer.testapp.BaseTest
+import com.bitflyer.testapp.data.CallResult
+import com.bitflyer.testapp.domain.userdetails.UserDetailsRepository
+import com.bitflyer.testapp.ui.userdetails.mapper.UserDetailsToUserDetailsModelMapper
+import com.bitflyer.testapp.ui.userdetails.model.UserDetailsModel
+import com.bitflyer.testapp.ui.userdetails.state.UserDetailsScreenState
+import com.bitflyer.testapp.userDetailsMock
+import com.bitflyer.testapp.userDetailsModelMock
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class UserDetailsViewModelTest : BaseTest() {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @MockK
+    private lateinit var repo: UserDetailsRepository
+
+    @MockK
+    private lateinit var mapper: UserDetailsToUserDetailsModelMapper
+
+    @RelaxedMockK
+    private lateinit var stateHandle: SavedStateHandle
+
+    @RelaxedMockK
+    private lateinit var observer: Observer<UserDetailsScreenState>
+
+    private val arguments = mutableListOf<UserDetailsScreenState>()
+
+    private lateinit var viewModel: UserDetailsViewModel
+
+    override fun setUp() {
+        super.setUp()
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        viewModel = UserDetailsViewModel(repo, mapper, stateHandle)
+        viewModel.screenState.observeForever(observer)
+        every { observer.onChanged(capture(arguments)) } answers { }
+    }
+
+    @Test
+    fun `savedStateHandle is not null, Loaded event is received`() {
+        every { stateHandle.get<UserDetailsModel>(any()) }.returns(userDetailsModelMock)
+        viewModel.fetchDetails("")
+        assertThat(arguments[0]).isInstanceOf(UserDetailsScreenState.Loaded::class.java)
+        assertThat((arguments[0] as UserDetailsScreenState.Loaded).model).isEqualTo(userDetailsModelMock)
+    }
+
+    @Test
+    fun `savedStateHandle is null and repo call is successful, Loading and Loaded events are received`() {
+        every { stateHandle.get<UserDetailsModel>(any()) }.returns(null)
+        coEvery { repo.getUserDetails(any()) }.returns(CallResult.Success(userDetailsMock))
+        every { mapper.map(any()) }.returns(userDetailsModelMock)
+        viewModel.fetchDetails("")
+        assertThat(arguments[0]).isInstanceOf(UserDetailsScreenState.Loading::class.java)
+        assertThat((arguments[1] as UserDetailsScreenState.Loaded).model).isEqualTo(userDetailsModelMock)
+    }
+
+    @Test
+    fun `savedStateHandle is null and repo call is not successful, Loading and Error events are received`() {
+        every { stateHandle.get<UserDetailsModel>(any()) }.returns(null)
+        coEvery { repo.getUserDetails(any()) }.returns(CallResult.IOError)
+        viewModel.fetchDetails("")
+        assertThat(arguments[0]).isInstanceOf(UserDetailsScreenState.Loading::class.java)
+        assertThat((arguments[1])).isInstanceOf(UserDetailsScreenState.Error::class.java)
+    }
+}
