99f2f81548fe59ecdf96f34ea32298321cd9f9b7
MaxNF <poeismylife@gmail.com>
2023-01-21 18:09:29 +0900

    add UserDetailsToUserDetailsModelMapper

---
+++ app/src/main/java/com/bitflyer/testapp/ui/userdetails/mapper/UserDetailsToUserDetailsModelMapper.kt
@@ -0,0 +1,23 @@
+package com.bitflyer.testapp.ui.userdetails.mapper
+
+import com.bitflyer.testapp.data.userdetails.dto.UserDetails
+import com.bitflyer.testapp.ui.BaseMapper
+import com.bitflyer.testapp.ui.userdetails.model.UserDetailsModel
+import javax.inject.Inject
+
+class UserDetailsToUserDetailsModelMapper @Inject constructor() : BaseMapper<UserDetails, UserDetailsModel> {
+    override fun map(item: UserDetails): UserDetailsModel = UserDetailsModel(
+        item.login,
+        item.avatarUrl,
+        item.name,
+        item.company,
+        item.blogUrl,
+        item.location,
+        item.email,
+        item.twitter,
+        item.bio,
+        item.followers.toString(),
+        item.following.toString(),
+        item.repos.toString()
+    )
+}
