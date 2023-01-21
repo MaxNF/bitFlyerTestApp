dc6df0bfc79443b26043726a48239af024c3cdb7
MaxNF <poeismylife@gmail.com>
2023-01-21 18:05:59 +0900

    add UserDetailsModel

---
+++ app/src/main/java/com/bitflyer/testapp/ui/userdetails/model/UserDetailsModel.kt
@@ -0,0 +1,20 @@
+package com.bitflyer.testapp.ui.userdetails.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UserDetailsModel(
+    val login: String,
+    val avatarUrl: String,
+    val name: String,
+    val company: String,
+    val blogUrl: String,
+    val location: String,
+    val email: String,
+    val twitter: String,
+    val bio: String,
+    val followers: String,
+    val following: String,
+    val repos: String
+): Parcelable
