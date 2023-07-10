cb4742904c8dc39fd6f0fb8464de256aabd8f0b4
MaxNF <poeismylife@gmail.com>
2023-07-10 23:14:10 +0900

    add top app bar

--- app/src/main/java/com/bitflyer/testapp/ui/BitflyerApp.kt
+++ app/src/main/java/com/bitflyer/testapp/ui/BitflyerApp.kt
@@ -1,22 +1,77 @@
 package com.bitflyer.testapp.ui
 
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.bitflyer.testapp.ui.compose.navigation.Destination
 import com.bitflyer.testapp.ui.compose.navigation.userDetailsScreen
 import com.bitflyer.testapp.ui.compose.navigation.userListScreen
 
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun BitflyerApp() {
     val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = Destination.UserList.route) {
+    val currentEntry = navController.currentBackStackEntryAsState()
+    Scaffold(
+        topBar = {
+            TopBar(currentEntry.value, navController)
+        }
+    ) {
+        NavHost(
+            modifier = Modifier
+                .padding(it)
+                .consumeWindowInsets(it), navController = navController, startDestination = Destination.UserList.route
+        ) {
             userListScreen { login ->
                 navController.navigate(
                     Destination.UserDetails.createRoute(login)
                 )
             }
             userDetailsScreen(navController::popBackStack)
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(entry: NavBackStackEntry?, navController: NavHostController) {
+    val currentRoute = entry?.destination?.route ?: Destination.UserList.route
+    TopAppBar(
+        navigationIcon = {
+            if (currentRoute != Destination.UserList.route) {
+                IconButton(onClick = navController::popBackStack) {
+                    Icon(imageVector = Icons.Rounded.ArrowBack, contentDescription = null)
+                }
+            }
+        },
+        title = {
+            when (currentRoute) {
+                Destination.UserList.route -> {
+                    Text("Github Users")
+                }
+
+                Destination.UserDetails.route -> {
+                    val login = navController.currentBackStackEntry?.arguments?.getString("login") ?: ""
+                    Row {
+                        Text(login)
+                    }
+                }
+            }
+        })
 }
--- app/src/main/java/com/bitflyer/testapp/ui/compose/userlist/UserListScreen.kt
+++ app/src/main/java/com/bitflyer/testapp/ui/compose/userlist/UserListScreen.kt
@@ -1,17 +1,28 @@
 package com.bitflyer.testapp.ui.compose.userlist
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import coil.compose.AsyncImage
 import com.bitflyer.testapp.data.local.UserBriefEntity
+import com.bitflyer.testapp.ui.compose.theme.BitflyerTheme
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
@@ -20,12 +31,54 @@ fun UserListScreen(
     list: Flow<PagingData<UserBriefEntity>>,
     onUserDetailsClick: (String) -> Unit,
 ) {
-    Surface {
-        Box(modifier = Modifier.fillMaxWidth()) {
-            Button(onClick = { onUserDetailsClick("MaxNF") }, modifier = Modifier.size(100.dp)) {
-                Text(text = "TEST")
+    val items = list.collectAsLazyPagingItems()
+    UserList(userBriefs = items, onUserDetailsClick = onUserDetailsClick)
+}
+
+@Composable
+fun UserList(
+    userBriefs: LazyPagingItems<UserBriefEntity>,
+    onUserDetailsClick: (String) -> Unit,
+) {
+    LazyColumn(contentPadding = PaddingValues(bottom = 32.dp)) {
+        items(userBriefs.itemCount) {
+            val userBrief = userBriefs[it]
+            userBrief?.let {
+                UserListItem(item = userBrief, onUserDetailsClick)
             }
         }
+    }
+}
+
+@Composable
+fun UserListItem(item: UserBriefEntity, onClick: (String) -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick(item.login) },
+    ) {
+        Row(modifier = Modifier.padding(horizontal = 16.dp), verticalAlignment = Alignment.CenterVertically) {
+            AsyncImage(
+                model = item.avatarUrl,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(shape = CircleShape)
+            )
+            Text(
+                text = item.login, textAlign = TextAlign.Center, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewUserListItem() {
+    BitflyerTheme {
+        UserListItem(item = UserBriefEntity(1, "MaxNF", "")) {}
     }
 }
 
